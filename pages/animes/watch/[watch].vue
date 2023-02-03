c8dc677d5f147e6cf6151a2893a37be5e80e8fa4
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-03 15:34:22 +0000

    Update [watch].vue

--- pages/animes/watch/[watch].vue
+++ pages/animes/watch/[watch].vue
@@ -8,16 +8,34 @@
                 <SpiningLoading></SpiningLoading>
             </div>
 
-            <div v-if="info" class="mb-4 text-white felx justify-center text-center py-4">
-                <h1>You are watching</h1>
-                <span class="text-sm font-bold text-purple-500">Episode {{ thisEp.number }}</span>
+            <div v-if="info" class="md:mb-4 text-white felx md:justify-center md:text-center py-4 px-5">
+                <h1 class="text-xl font-black md:hidden mb-3">
+                    <NuxtLink :to="'/animes/' + info.id">{{ info.title }}</NuxtLink>
+                </h1>
+                <h1 class=" text-zinc-300 inline-block text-sm md:text-base md:block md:text-white">
+                    You are watching
+                </h1>
+                <span class="text-sm font-bold inline-block md:block ml-2 md:ml-0 text-purple-500">
+                    Episode {{ thisEp.number }}
+                </span>
+            </div>
+
+            <div class="w-11/12 mx-auto py-3 px-5 text-white bg-slate-800 rounded-xl md:hidden">
+                <div class="font-bold">Genres:</div>
+                <div v-for="g in info.genres" :key="g" class="inline-block mr-2 text-sm" :id="g"
+                    :style="'color:' + randomColor(g)">
+                    {{ g }}
+                </div>
             </div>
 
             <div v-if="info != null" class="mt-5 mx-auto w-full lg:w-3/4 container px-3 flex gap-3">
-                <div class="w-1/5">
+                <div class="w-full md:w-1/5 mb-5">
                     <img :src="info.image" alt="" srcset="" class="w-full">
                 </div>
                 <div class="text-white w-4/5">
+                    <h1 class="mb-3 text-lg block lg:hidden lg:text-2xl">
+                        {{ info.otherName }}
+                    </h1>
                     <h1 class="mb-3 text-lg lg:text-2xl">
                         <NuxtLink :to="'/animes/' + info.id">{{ info.title }}</NuxtLink>
                     </h1>
@@ -94,7 +112,7 @@ export default {
             await fetch(api)
                 .then(response => response.json())
                 .then(data => {
-                    // console.log('data:', data)
+                    console.log('data:', data)
                     this.sortEpisode(data.episodes)
                     this.info = data;
                     this.shuffle(data.genres)
@@ -226,6 +244,20 @@ export default {
         shuffle(array) {
             array.sort(() => Math.random() - 0.5);
         },
+        filterFilter(arr, expression) {
+            return arr.filter(function (item) {
+                return item[Object.keys(expression)[0]] == Object.values(expression)[0];
+            });
+        },
+        randomColor() {
+            var letters = 'BCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * letters.length)];
+            }
+            return color;
+            // return '#' + Math.floor(Math.random() * 16777215).toString(16);
+        }
     }
 }
 </script>
