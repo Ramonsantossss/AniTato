8958a72d40db95921c419860fb536ac823d9671a
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-07 01:30:41 +0000

    remove autofocus

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -31,7 +31,7 @@
                     </div>
                     <input type="text" name="price" id="price"
                         class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
-                        placeholder="Search anime ..." v-model="searchQuery" autofocus>
+                        placeholder="Search anime ..." v-model="searchQuery">
 
                 </div>
             </div>
