69352b9b1e4d5153c1ca51c7e325f9501fcd60bf
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-07 14:21:00 +0000

    Testing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,4 +1,53 @@
 <template>
+    <div class="z-50 bg-slate-900">
+        <nav class="w-full shadow-md">
+            <ul class="flex items-center justify-between container list-none px-2 py-4 mx-auto">
+                <li class="flex items-center gap-3">
+                    <button @click="toggleSearch" v-show="!showSearch" class="mr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                            class="w-6 h-6 text-white font-extrabold">
+                            <path fillRule="evenodd"
+                                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+
+        <transition enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-out" leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0">
+            <div class="absolute w-full" v-show="showSearch">
+                <div class="flex relative justify-center container mx-auto">
+                    <input type="text" name="price" id="price"
+                        class="block w-full rounded-md border-gray-300 pl-7 pr-12 py-3 shadow-md focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Search anime ..." v-model="searchQuery" autocomplete="off" autofocus>
+                    <div class="absolute inset-y-0 right-0 flex items-center">
+                        <button class=" p-2" @click="searchAnime">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="w-6 h-6 font-extrabold">
+                                <path fillRule="evenodd"
+                                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                                    clipRule="evenodd" />
+                            </svg>
+                        </button>
+                        <button class=" p-2 text-red-500" @click="toggleSearch">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+<!--
+<template>
     <nav class="z-50 dark:bg-gray-900 sticky top-0 w-full shadow-md">
         
         <ul class="flex items-center justify-between container list-none px-2 py-4 mx-auto">
@@ -44,7 +93,7 @@
             </li>
         </ul>
     </nav>
-    <!--
+    
     <div class="">
         <transition enter-active-class="transition duration-100 ease-out"
             enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
@@ -75,8 +124,8 @@
                 </div>
             </div>
         </transition>
-    </div>-->
-</template>
+    </div>
+</template>-->
 
 <script>
 
@@ -155,11 +204,11 @@ export default {
 }
 
 .fadeInFromLeft {
-    animation: moveInFromLeft 0.4s ease-out;
+    animation: moveInFromLeft 0.25s ease-out;
 }
 
 .fadeInFromRight {
-    animation: moveInFromRight 0.4s ease-out;
+    animation: moveInFromRight 0.25s ease-out;
 }
 
 /* animation move in from left */
