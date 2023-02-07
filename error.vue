5165392897b9bc62f09ebf17953a83f12d328672
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-07 14:23:24 +0000

    <img src="/astronaut.png" alt="" /> REMOVED!!

--- error.vue
+++ error.vue
@@ -21,7 +21,6 @@
             class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
             <span class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
           <!-- DON T FORGET TO CHANGE THE IMG -->
-            <img src="/astronaut.png" alt="" />
             <span class="relative block px-8 py-3 bg-[#1A2238] border border-current">
                 <a href="/">Go Home</a>
                     </span>
