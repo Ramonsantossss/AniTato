ac230151e1ca0bdce76b16a950f38ba0ef62b5da
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-07 10:35:06 +0000

    3rd API try

---
+++ pages/manga/[manga]/[volume].vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="chapter">
+        <img v-for="c in chapter" :key="c" :src="c.img" alt="">
+    </div>
+    <div v-else class="w-full min-h-screen flex justify-center mt-5">
+        <SpiningLoading></SpiningLoading>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            chapter: null,
+            loading: true,
+        }
+    },
+    mounted() {
+        this.getChapter()
+    },
+    methods: {
+        async getChapter() {
+            await fetch('https://shadow-anime-api.vercel.app/manga/mangapark/read?chapterId=' + this.$route.params.manga + '/' + this.$route.params.volume)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    this.chapter = data
+                    this.loading = false
+                })
+        },
+    },
+}
+</script>
+<style>
+</style>
