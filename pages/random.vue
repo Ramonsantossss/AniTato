a1460a02c67b9a3d9507c1d93ce477c8b4970304
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-10 16:04:15 +0000

    revert changes.. gonna work in the morning..

--- pages/random.vue
+++ pages/random.vue
@@ -162,14 +162,13 @@ export default {
             this.serverUrl = "https://zoro.to"
         }
 
-        fetch('https://api.jikan.moe/v4/random/anime')
+        fetch(url)
             .then(response => response.json())
             .then(data => {
                 // console.log(data)
                 if (data.id != null) {
-                    var code=data.data.title.toLowerCase().replace(/ /g, '-');
                     this.anime.id = data.id;
-                    this.anime.title = code;
+                    this.anime.title = data.title;
                     this.anime.otherName = data.otherName
                     this.anime.description = data.description;
                     this.anime.url = data.url;
