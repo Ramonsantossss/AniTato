de4313230a5ccf802164ae013f3f9bdab0266c53
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-11 09:55:40 +0000

    finish random setup

--- pages/test.vue
+++ pages/test.vue
@@ -2,31 +2,15 @@
   <div class="container p-4 mx-auto min-h-screen">
       <div class="text-white my-4 min-h-[20vh] flex items-center">
           <h1 class="text-4xl font-extrabold">
-              Redirecting...
+              Test
           </h1>
       </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
-  data() {
-    return {
-      animeTitle: ''
-    }
-    },
-    created() {
-    axios.get('https://api.jikan.moe/v4/random/anime')
-      .then(response => {
-        var title = response.data.data.title.toLowerCase().replace(/ /g, '-');
-        this.$router.push('/animes/' + title)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-    },
+
 }
 </script>
 
