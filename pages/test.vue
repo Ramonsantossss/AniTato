a7c26909e5a69d5c6a9958ba2a6bb53dc457aceb
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-11 09:09:00 +0000

    revert all changes

--- pages/test.vue
+++ pages/test.vue
@@ -1,13 +1,27 @@
 <template>
-    <div class=" min-h-screen bg-black">
-        <h1 class="text-white">Test</h1>
-        <p>Test page</p>
+    <div class="min-h-screen">
+        <h1 class="text-white">{{ animeTitle }}</h1>
     </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      animeTitle: ''
+    }
+    },
+    created() {
+    axios.get('https://api.jikan.moe/v4/random/anime')
+      .then(response => {
+        this.animeTitle = response.data.data.title.toLowerCase().replace(/ /g, '-');
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    },
 }
 </script>
 
