02ed0c18b5ee37d6cf8d0ed987cdc2b266a189e6
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-11 06:59:09 +0000

    Testing some code

---
+++ components/MainMasterpage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <div>
+      </div>
+      <div>
+        <slot>
+          <h1>Super Nika!</h1>
+        </slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+export default {
+    name: 'MainMasterpage',
+  }
+  </script>
+
+  <style lang="css" scoped> 
+</style>
+  
